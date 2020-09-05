ebbc8185e361fab5881aff9b2f7c7f8afbb213f4
oceans <oceans-mail@qq.com>
2020-09-05 14:52:40 +0800

    log 完善

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-from time import sleep
-import time
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import json
-
+import time
+from datetime import datetime
+from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def document_initialised(driver):
-    return driver.execute_script("return initialised")
+def log(file, data):
+    success_time = datetime.now()
+    file.write(str(success_time) + '\n')
+    file.write(data + '\n')
+
+
+def log_data(driver, file):
+    '''
+    1. 填报日期
+    2. 姓名
+    3. 学工号
+    4. 地址 *
+    5. 范围 *
+    6. 是否在校
+    7. 是否外出
+    8. 是否隔离
+    9. 其他情况  （不填）
+    '''
+    key_arr = ['填报日期', '姓名', '学工号', '地址', '温度范围', '是否在校', '是否外出', '是否隔离']
+    document = driver
+    value_arr = []
+    input_list = document.find_elements_by_tag_name('input')
+    for dom_input in input_list:
+        value = dom_input.get_attribute('value')
+        value_arr.append(value)
+
+    span_list = document.find_elements_by_css_selector('.active+span')
+    for dom_span in span_list:
+        value = dom_span.text
+        value_arr.append(value)
+
+    # 拼接JSON
+    result_map = {}
+    for i in range(8):
+        result_map[key_arr[i]] = value_arr[i]
+    print(result_map)
+
+    # 将cookies保存为json格式
+
+    log(file, '4. 当前填报信息为\n' + json.dumps(result_map, ensure_ascii=False) + '\r\n')
+    driver.quit()
 
 
 def auto_submit(username, password):
+    filename = 'report/' + username + '.log'
+    file = open(filename, 'a', encoding='utf-8')
+
     print('0. 执行脚本')
+    log(file, '0. 执行脚本')
     # 将chromedriver放到python脚本的文件夹下面，比如（`Anaconda3\Scripts`）
     # 就可以不用每次手动指定目录了
     # driver = webdriver.Chrome('d:/software/dev/chromedriver.exe')
@@ -47,6 +86,8 @@ def auto_submit(username, password):
     driver.execute_script(js)
     WebDriverWait(driver, 10).until(lambda d: d.find_element_by_class_name('btn'))
     print('1. 进入登录页面')
+    log(file, '1. 进入登录页面')
+
     dom_username = document.find_elements_by_tag_name('input')[0]
     dom_password = document.find_elements_by_tag_name('input')[1]
     dom_login_btn = document.find_element_by_class_name('btn')
@@ -54,16 +95,19 @@ def auto_submit(username, password):
     dom_username.send_keys(username)
     dom_password.send_keys(password)
     dom_login_btn.click()
-    sleep(1.5)
+    time.sleep(1.5)
     try:
         document.find_element_by_class_name('wapat-title')
-        print('用户名或密码错误')
+        print('【用户名或密码错误】')
+        log(file, '【用户名或密码错误】')
         return driver.quit()
     except NoSuchElementException:
 
         # 是否已经填报过？
         driver.implicitly_wait(10)
+        time.sleep(1)
         print('2. 进入填报页面')
+        log(file, '2. 进入填报页面')
         dom_submit_btn = document.find_element_by_css_selector('.footers a')
         tip = dom_submit_btn.text
         need_commit = (tip.find('已提交过') != -1) and (tip.find('未到填报时间') != -1)
@@ -74,50 +118,22 @@ def auto_submit(username, password):
             dom_btn_pos = document.find_element_by_css_selector('[name=area] input')
             dom_btn_tw.click()
             dom_btn_pos.click()
+            time.sleep(1)
             # check
-
-            # print('3. 提交成功')
-            # dom_submit_btn.click()
+            try:
+                # dom_submit_btn.click()
+                print('3. 提交成功')
+                log(file, '3. 提交成功')
+                log_data(driver, file)
+            except Exception as ex:
+                print('4. 提交失败')
+                log(file, '3. 提交失败')
+                log(file, str(ex))
         else:
-            print(tip)
-
-        # check data
-        '''
-        1. 填报日期
-        2. 姓名
-        3. 学工号
-        4. 地址 *
-        5. 范围 *
-        6. 是否在校
-        7. 是否外出
-        8. 是否隔离
-        9. 其他情况  （不填）
-        '''
-
-        key_arr = ['填报日期', '姓名', '学工号', '地址', '温度范围', '是否在校', '是否外出', '是否隔离']
-        value_arr = []
-        input_list = document.find_elements_by_tag_name('input')
-        for dom_input in input_list:
-            value = dom_input.get_attribute('value')
-            value_arr.append(value)
-
-        span_list = document.find_elements_by_css_selector('.active+span')
-        for dom_span in span_list:
-            value = dom_span.text
-            value_arr.append(value)
-
-        # 拼接JSON
-        result_map = {}
-        for i in range(8):
-            result_map[key_arr[i]] = value_arr[i]
-        print(result_map)
-
-        filename = 'report/' + username + '.log'
-        # 将cookies保存为json格式
-        file = open(filename, 'a', encoding='utf-8')
-        file.write(json.dumps(result_map, ensure_ascii=False) + '\r\n')
+            print('3. 不需要提交 ：' + tip)
+            log(file, '3. 不需要提交 ：' + tip)
+            log_data(driver, file)
         file.close()
-        driver.quit()
 
 
 if __name__ == '__main__':
